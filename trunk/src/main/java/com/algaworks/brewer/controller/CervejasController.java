46d1f54c1cb76da2c406052c684e6ba69519e826
ailtonluiz <ailtonluiz@1f8b9598-982f-4940-8909-1e9cae4bf51e>
2019-03-09 16:34:51 +0000

    Configuração Flyway

--- trunk/src/main/java/com/algaworks/brewer/controller/CervejasController.java
+++ trunk/src/main/java/com/algaworks/brewer/controller/CervejasController.java
@@ -2,6 +2,7 @@ package com.algaworks.brewer.controller;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -10,12 +11,18 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.algaworks.brewer.model.Cerveja;
+import com.algaworks.brewer.repository.Cervejas;
 
 @Controller
 public class CervejasController {
 
+	@Autowired
+	private Cervejas cervejas;
+	
 	@RequestMapping("/cervejas/novo")
 	public String novo(Cerveja cerveja) {
+		cervejas.findAll(); // Apagar...
+		
 		return "cerveja/CadastroCerveja";
 	}
 	
@@ -31,12 +38,5 @@ public class CervejasController {
 		return "redirect:/cervejas/novo";
 	}
 	
-
-	@RequestMapping("/cervejas/cadastro")
-	public String cadastro() {
-		return "cerveja/cadastro-produto";
-	}
 	
 }
-
-    
