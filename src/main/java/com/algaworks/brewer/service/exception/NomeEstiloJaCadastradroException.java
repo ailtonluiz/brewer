462d963b8ed476e8e62b3ddf3e417307316272df
ailtonluiz <ailtonluiz@1f8b9598-982f-4940-8909-1e9cae4bf51e>
2019-03-13 06:37:42 +0000

    Implementado o cadastro de estilos

---
+++ src/main/java/com/algaworks/brewer/service/exception/NomeEstiloJaCadastradroException.java
@@ -0,0 +1,11 @@
+package com.algaworks.brewer.service.exception;
+
+public class NomeEstiloJaCadastradroException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public NomeEstiloJaCadastradroException(String message) {
+		super(message);
+	}
+
+}
