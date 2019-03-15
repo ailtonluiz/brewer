ce931fec65f9f32348ecb2b4df7f68f24b7bbfca
ailtonluiz <ailtonluiz@1f8b9598-982f-4940-8909-1e9cae4bf51e>
2019-03-15 20:14:59 +0000

    Salvando a foto

--- trunk/src/main/java/com/algaworks/brewer/storage/local/FotoStorageLocal.java
+++ trunk/src/main/java/com/algaworks/brewer/storage/local/FotoStorageLocal.java
@@ -6,10 +6,13 @@
 package com.algaworks.brewer.storage.local;
 
 import static java.nio.file.FileSystems.getDefault;
+
+import java.io.File;
 import java.io.IOException;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.UUID;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -38,6 +41,32 @@ public class FotoStorageLocal implements FotoStorage {
 		criarPastas();
 	}
 
+	@Override
+	public String salvarTemporariamente(MultipartFile[] files) {
+		String novoNome = null;
+		if (files != null && files.length > 0) {
+			MultipartFile arquivo = files[0];
+			novoNome = renomearArquivo(arquivo.getOriginalFilename());
+			try {
+				arquivo.transferTo(new File(
+						this.localTemporario.toAbsolutePath().toString() + getDefault().getSeparator() + novoNome));
+			} catch (IOException e) {
+				throw new RuntimeException("Erro salvando a foto na pasta temporária " + e);
+			}
+		}
+		return novoNome;
+	}
+
+	@Override
+	public byte[] recuperarFotoTemporaria(String nome) {
+
+		try {
+			return Files.readAllBytes(this.localTemporario.resolve(nome));
+		} catch (IOException e) {
+			throw new RuntimeException("Erro ao recuperar a foto " + e);
+		}
+	}
+
 	private void criarPastas() {
 		try {
 			Files.createDirectories(this.local);
@@ -52,18 +81,15 @@ public class FotoStorageLocal implements FotoStorage {
 		} catch (IOException e) {
 			throw new RuntimeException("Erro criando pasta para salvar foto ", e);
 		}
-
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.algaworks.brewer.storage.FotoStorage#salvarTemporariamente(org.
-	 * springframework.web.multipart.MultipartFile[])
-	 */
-	@Override
-	public void salvarTemporariamente(MultipartFile[] files) {
-		System.out.println(">>>>> salvando a foto temporariamente ");
+	private String renomearArquivo(String nomeOriginal) {
+		String novoNome = UUID.randomUUID().toString() + "_" + nomeOriginal;
 
+		if (logger.isDebugEnabled()) {
+			logger.debug(String.format("Novo original: %s, noov nome: %s", nomeOriginal, novoNome));
+		}
+		return novoNome;
 	}
+
 }
