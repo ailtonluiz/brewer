f413aa5af851bc3f49e41e718586a3999f770305
ailtonluiz <ailtonluiz@1f8b9598-982f-4940-8909-1e9cae4bf51e>
2019-05-20 20:52:50 +0000

    Removido os imports obsoletos

--- trunk/src/main/java/com/algaworks/brewer/storage/local/FotoStorageLocal.java
+++ trunk/src/main/java/com/algaworks/brewer/storage/local/FotoStorageLocal.java
@@ -1,15 +1,9 @@
-/**
- * 14 de mar de 2019
- * brewer
- * ailto
- */
 package com.algaworks.brewer.storage.local;
 
 import static java.nio.file.FileSystems.getDefault;
 
 import java.io.File;
 import java.io.IOException;
-
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.UUID;
@@ -20,26 +14,20 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.algaworks.brewer.storage.FotoStorage;
 
-import net.coobird.thumbnailator.Thumbnailator;
 import net.coobird.thumbnailator.Thumbnails;
 import net.coobird.thumbnailator.name.Rename;
 
-/**
- * @author ailto
- *
- */
 public class FotoStorageLocal implements FotoStorage {
 
 	private static final Logger logger = LoggerFactory.getLogger(FotoStorageLocal.class);
-
+	
 	private Path local;
 	private Path localTemporario;
-
+	
 	public FotoStorageLocal() {
-		this(getDefault().getPath(System.getProperty("user.home"), ".brewerfotos"));
-
+		this(getDefault().getPath(System.getenv("HOME"), ".brewerfotos"));
 	}
-
+	
 	public FotoStorageLocal(Path path) {
 		this.local = path;
 		criarPastas();
@@ -52,76 +40,73 @@ public class FotoStorageLocal implements FotoStorage {
 			MultipartFile arquivo = files[0];
 			novoNome = renomearArquivo(arquivo.getOriginalFilename());
 			try {
-				arquivo.transferTo(new File(
-						this.localTemporario.toAbsolutePath().toString() + getDefault().getSeparator() + novoNome));
+				arquivo.transferTo(new File(this.localTemporario.toAbsolutePath().toString() + getDefault().getSeparator() + novoNome));
 			} catch (IOException e) {
-				throw new RuntimeException("Erro salvando a foto na pasta temporária " + e);
+				throw new RuntimeException("Erro salvando a foto na pasta temporária", e);
 			}
 		}
+		
 		return novoNome;
 	}
-
+	
 	@Override
 	public byte[] recuperarFotoTemporaria(String nome) {
-
 		try {
 			return Files.readAllBytes(this.localTemporario.resolve(nome));
 		} catch (IOException e) {
-			throw new RuntimeException("Erro ao recuperar a foto temporaria " + e);
+			throw new RuntimeException("Erro lendo a foto temporária", e);
 		}
-
 	}
-
+	
 	@Override
 	public void salvar(String foto) {
 		try {
 			Files.move(this.localTemporario.resolve(foto), this.local.resolve(foto));
 		} catch (IOException e) {
-			throw new RuntimeException("Erro movendo foto para destino final", e);
+			throw new RuntimeException("Erro movendo a foto para destino final", e);
 		}
-
+		
 		try {
 			Thumbnails.of(this.local.resolve(foto).toString()).size(40, 68).toFiles(Rename.PREFIX_DOT_THUMBNAIL);
 		} catch (IOException e) {
-
 			throw new RuntimeException("Erro gerando thumbnail", e);
 		}
-
 	}
-
+	
 	@Override
 	public byte[] recuperar(String nome) {
-
 		try {
 			return Files.readAllBytes(this.local.resolve(nome));
 		} catch (IOException e) {
-			throw new RuntimeException("Erro ao recuperar a foto " + e);
+			throw new RuntimeException("Erro lendo a foto", e);
 		}
 	}
-
+	
 	private void criarPastas() {
 		try {
 			Files.createDirectories(this.local);
 			this.localTemporario = getDefault().getPath(this.local.toString(), "temp");
 			Files.createDirectories(this.localTemporario);
-
+			
 			if (logger.isDebugEnabled()) {
-				logger.debug("Pastas criadas para salvar fotos. ");
+				logger.debug("Pastas criadas para salvar fotos.");
 				logger.debug("Pasta default: " + this.local.toAbsolutePath());
 				logger.debug("Pasta temporária: " + this.localTemporario.toAbsolutePath());
 			}
 		} catch (IOException e) {
-			throw new RuntimeException("Erro criando pasta para salvar foto ", e);
+			throw new RuntimeException("Erro criando pasta para salvar foto", e);
 		}
 	}
-
+	
 	private String renomearArquivo(String nomeOriginal) {
 		String novoNome = UUID.randomUUID().toString() + "_" + nomeOriginal;
-
+		
 		if (logger.isDebugEnabled()) {
-			logger.debug(String.format("Novo original: %s, novo nome: %s", nomeOriginal, novoNome));
+			logger.debug(String.format("Nome original: %s, novo nome: %s", nomeOriginal, novoNome));
 		}
+		
 		return novoNome;
+		
 	}
 
 }
