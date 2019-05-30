d2e17aab9d886152b1582eaf2357c366a7d6c763
ailtonluiz <ailtonluiz@1f8b9598-982f-4940-8909-1e9cae4bf51e>
2019-05-30 09:30:43 +0000

    Adicionado relatórios Relatório de orçamentos Relatório de vendas canceladas

--- trunk/src/main/java/com/algaworks/brewer/controller/RelatoriosController.java
+++ trunk/src/main/java/com/algaworks/brewer/controller/RelatoriosController.java
@@ -18,28 +18,74 @@ import com.algaworks.brewer.dto.PeriodoRelatorio;
 @Controller
 @RequestMapping("/relatorios")
 public class RelatoriosController {
-	
+
 	@GetMapping("/vendasEmitidas")
 	public ModelAndView relatorioVendasEmitidas() {
 		ModelAndView mv = new ModelAndView("relatorio/RelatorioVendasEmitidas");
 		mv.addObject(new PeriodoRelatorio());
 		return mv;
 	}
-	
+
+	@GetMapping("/orcamentosEmitidos")
+	public ModelAndView relatorioOrcamentoEmitidos() {
+		ModelAndView mv = new ModelAndView("relatorio/RelatorioOrcamentosEmitidos");
+		mv.addObject(new PeriodoRelatorio());
+		return mv;
+	}
+
+	@GetMapping("/vendasCanceladas")
+	public ModelAndView relatorioVendasCanceladas() {
+		ModelAndView mv = new ModelAndView("relatorio/RelatorioVendasCanceladas");
+		mv.addObject(new PeriodoRelatorio());
+		return mv;
+	}
+
 	@PostMapping("/vendasEmitidas")
 	public ModelAndView gerarRelatorioVendasEmitidas(PeriodoRelatorio periodoRelatorio) {
 		Map<String, Object> parametros = new HashMap<>();
-		
+
 		Date dataInicio = Date.from(LocalDateTime.of(periodoRelatorio.getDataInicio(), LocalTime.of(0, 0, 0))
 				.atZone(ZoneId.systemDefault()).toInstant());
 		Date dataFim = Date.from(LocalDateTime.of(periodoRelatorio.getDataFim(), LocalTime.of(23, 59, 59))
 				.atZone(ZoneId.systemDefault()).toInstant());
-		
+
 		parametros.put("format", "pdf");
 		parametros.put("data_inicio", dataInicio);
 		parametros.put("data_fim", dataFim);
-		
+
 		return new ModelAndView("relatorio_vendas_emitidas", parametros);
+	}
+
+	@PostMapping("/orcamentosEmitidos")
+	public ModelAndView gerarRelatorioOrcamentoEmitidos(PeriodoRelatorio periodoRelatorio) {
+		Map<String, Object> parametros = new HashMap<>();
+
+		Date dataInicio = Date.from(LocalDateTime.of(periodoRelatorio.getDataInicio(), LocalTime.of(0, 0, 0))
+				.atZone(ZoneId.systemDefault()).toInstant());
+		Date dataFim = Date.from(LocalDateTime.of(periodoRelatorio.getDataFim(), LocalTime.of(23, 59, 59))
+				.atZone(ZoneId.systemDefault()).toInstant());
+
+		parametros.put("format", "pdf");
+		parametros.put("data_inicio", dataInicio);
+		parametros.put("data_fim", dataFim);
+
+		return new ModelAndView("relatorio_orcamentos", parametros);
+	}
+
+	@PostMapping("/vendasCanceladas")
+	public ModelAndView gerarRelatorioVendasCanceladas(PeriodoRelatorio periodoRelatorio) {
+		Map<String, Object> parametros = new HashMap<>();
+
+		Date dataInicio = Date.from(LocalDateTime.of(periodoRelatorio.getDataInicio(), LocalTime.of(0, 0, 0))
+				.atZone(ZoneId.systemDefault()).toInstant());
+		Date dataFim = Date.from(LocalDateTime.of(periodoRelatorio.getDataFim(), LocalTime.of(23, 59, 59))
+				.atZone(ZoneId.systemDefault()).toInstant());
+
+		parametros.put("format", "pdf");
+		parametros.put("data_inicio", dataInicio);
+		parametros.put("data_fim", dataFim);
+
+		return new ModelAndView("relatorio_vendas_canceladas", parametros);
 	}
 
 }
